d8f608c21b3736fa54cb453ca800aa36797004e9
xj894976677 <894976677@qq.com>
2019-09-16 08:49:28 +0800

    下载图片

---
+++ Test/picutre.py
@@ -0,0 +1,13 @@
+# import urllib
+#
+# import requests
+# headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"}
+# response = requests.get('http://pic.topmeizi.com/wp-content/uploads/2011a/12/49/06.jpg', headers=headers)
+#
+# print(response.content)
+# with open('1.jpg', 'wb') as f:
+#     f.write(response.content)
+if not False:
+    print("ok")
+else:
+    print("true")
